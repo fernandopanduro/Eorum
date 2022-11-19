6266e89f32ff244493c4aa32ef3fefd0c1bc75a4
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-11-18 18:38:15 -0700

    Mejorada la pagina de contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,23 +2,28 @@ import styles from '../css/Contact.module.css';
 
 export function Contact() {
     return (
-        <section className={`${styles.formContainer} container`}>
+        <section className={`container`}>
+            <div className='ruta'>
+                Home / Contact
+            </div>
+            <h2 className={styles.title}>Contact</h2>
 
 
             <form action="" className={styles.form}>
                 <fieldset className={styles.formFieldset}>
-                    <h1 className={styles.title}>Contact</h1>
+                    <p>Questions? Comments? Get in touch with you by filling out the email form below. We will get back to them within 24 hours. Email: support@eorum.com</p>
+                    <h3 className={styles.subtitle}>Send us an email</h3>
+                    <div className={styles.containerLabel}>
+                        <label className={styles.formLabel} htmlFor="asunto">Name:</label>
+                        <input className={styles.formInput} type="text" name="asunto" id="asunto"/>
+                    </div>
                     <div className={styles.containerLabel}>
                         <label className={styles.formLabel} htmlFor="email">Email:</label>
-                        <input className={styles.formInput} type="email" id="email" placeholder="Exemple@email.com"/>
+                        <input className={styles.formInput} type="email" id="email"/>
                     </div>
                     <div className={styles.containerLabel}>
-                        <label className={styles.formLabel} htmlFor="asunto">Asunto:</label>
-                        <input className={styles.formInput} type="text" name="asunto" id="asunto" placeholder="Help me..."/>
-                    </div>
-                    <div className={styles.containerLabel}>
-                        <label htmlFor="">Menssage</label>
-                        <textarea className={styles.formInput} name="" id="" cols="30" rows="10" placeholder='Menssage...'></textarea>
+                        <label className={styles.formLabel} htmlFor="">Menssage:</label>
+                        <textarea className={styles.formInput} name="" id="" cols="30" rows="10"></textarea>
                     </div>
                 </fieldset>
                 <input className={styles.formButton} type="submit" value="Submit" />
--- src/Pages/Refund.jsx
+++ src/Pages/Refund.jsx
@@ -9,7 +9,7 @@ export function Refund() {
             <h2 className={styles.refundTitle}>Refund</h2>
             <div className={styles.refundContainer}>
                 <div className={styles.paragrathContainer}>
-                    <p>Returns</p>
+                    <p className={styles.paragrathTitle}>Returns</p>
                     <p>Our policy lasts 30 days. If 30 days have gone by since your purchase, unfortunately we can’t offer you a refund or exchange.</p>
                 </div>
                 <div className={styles.paragrathContainer}>
@@ -24,35 +24,35 @@ export function Refund() {
                     <p>Any item that is returned more than 30 days after delivery</p>
                 </div>
                 <div className={styles.paragrathContainer}>
-                    <p>Refunds (if applicable)</p>
+                    <p className={styles.paragrathTitle}>Refunds (if applicable)</p>
                     <p>Once your return is received and inspected, we will send you an email to notify you that we have received your returned item. We will also notify you of the approval or rejection of your refund.</p>
                     <p>If you are approved, then your refund will be processed, and a credit will automatically be applied to your credit card or original method of payment, within a certain amount of days.</p>
                 </div>
                 <div className={styles.paragrathContainer}>
-                    <p>Late or missing refunds (if applicable)</p>
+                    <p className={styles.paragrathTitle}>Late or missing refunds (if applicable)</p>
                     <p>If you haven’t received a refund yet, first check your bank account again.</p>
                     <p>Then contact your credit card company, it may take some time before your refund is officially posted.</p>
                     <p>Next contact your bank. There is often some processing time before a refund is posted.</p>
                     <p>If you’ve done all of this and you still have not received your refund yet, please contact us at support@eorum.com.</p>
                 </div>
                 <div className={styles.paragrathContainer}>
-                    <p>Sale items (if applicable)</p>
+                    <p className={styles.paragrathTitle}>Sale items (if applicable)</p>
                     <p>Only regular priced items may be refunded, unfortunately sale items cannot be refunded.
                     Only regular priced items may be refunded, unfortunately sale items cannot be refunded.</p>
                 </div>
                 <div className={styles.paragrathContainer}>
-                    <p>Exchanges (if applicable)</p>
+                    <p className={styles.paragrathTitle}>Exchanges (if applicable)</p>
                     <p>If you need to exchange it for the same item for larger or smaller size, send us an email at support@koisea.com</p>
                 </div>
                 <div className={styles.paragrathContainer}>
-                    <p>Gifts</p>
+                    <p className={styles.paragrathTitle}>Gifts</p>
                     <p>If the item was marked as a gift when purchased and shipped directly to you, you’ll receive a gift credit for the value of your return. Once the returned item is received, a gift certificate will be mailed to you.</p>
                 </div>
                 <div className={styles.paragrathContainer}>
                     <p>If the item wasn’t marked as a gift when purchased, or the gift giver had the order shipped to themselves to give to you later, we will send a refund to the gift giver and he will find out about your return.</p>
                 </div>
                 <div className={styles.paragrathContainer}>
-                    <p>Shipping</p>
+                    <p className={styles.paragrathTitle}>Shipping</p>
                     <p>To return your product, you should email us at support@koisea.com first.</p>
                 </div>
                 <div className={styles.paragrathContainer}>
