8cdfc61cf36953eb77ca31471b794be7564705a4
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-12-05 19:24:14 -0700

    Agregado animacion de color al banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,29 @@ export function Banner() {
             <div className={styles.bannerContainer}>    
                 <h2 className={styles.title}>Japonese Street Fashion</h2>
                 <div>
-                    <p className={styles.subtitle}>Being Bold  And Creative</p>
+                    <p className={styles.subtitle}>
+                        <span>B</span>
+                        <span>e</span>
+                        <span>i</span>
+                        <span>n</span>
+                        <span>g</span>
+                        <span> B</span>
+                        <span>o</span>
+                        <span>l</span>
+                        <span>d</span>
+                        <span>  A</span>
+                        <span>n</span>
+                        <span>d</span>
+                        <span> C</span>
+                        <span>r</span>
+                        <span>e</span>
+                        <span>a</span>
+                        <span>t</span>
+                        <span>i</span>
+                        <span>v</span>
+                        <span>e</span>
+
+                        </p>
                     <button className={styles.btn}><Link to='/new'>Shop Now</Link></button>
                 </div>
             </div>
